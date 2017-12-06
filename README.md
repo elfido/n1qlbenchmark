2552f480a8ffca5d79538fa79dc0058505ff81f5
Fidencio <fg0217@att.com>
2017-12-05 22:32:44 -0800

    Adding README

---
+++ README.md
@@ -0,0 +1,13 @@
+# N1QL Benchmark
+
+## Usage
+
+Execute profiling session
+```sh
+./n1qlb -c 1 -i config.json
+```
+
+All flags and options are available with the following command:
+```sh
+./n1qlb -help
+```
--- compile.sh
+++ compile.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo Creating binaries
 echo Generating Linux binary
-GOOS=linux GOARCH=amd64 go build -o ./compiled/linux/cbmon *.go
+GOOS=linux GOARCH=amd64 go build -o ./compiled/linux/n1qlb *.go
 echo Generating OSX binary
-GOOS=darwin GOARCH=amd64 go build -o ./compiled/osx/cbmon *.go
+GOOS=darwin GOARCH=amd64 go build -o ./compiled/osx/n1qlb *.go
 echo Generating Windows binary
-GOOS=windows GOARCH=amd64 go build -o ./compiled/win/cbmon.exe *.go
+GOOS=windows GOARCH=amd64 go build -o ./compiled/win/n1qlb.exe *.go
 echo Done!
